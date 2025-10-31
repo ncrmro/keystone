731e2f8d2e71373145eb00e867955c69f4524349
Nicholas Romero <ncrmro@gmail.com>
2025-10-31 10:40:21 -0500

    refactor: test deployment to use new ssh based unlock

--- flake.nix
+++ flake.nix
@@ -17,56 +17,56 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      disko,
-      home-manager,
-      omarchy,
-      ...
-    }:
-    {
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+  outputs = {
+    self,
+    nixpkgs,
+    disko,
+    home-manager,
+    omarchy,
+    ...
+  }: {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
 
-      # ISO configuration without SSH keys (use bin/build-iso for SSH keys)
-      nixosConfigurations = {
-        keystoneIso = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-            ./modules/iso-installer.nix
-            {
-              _module.args.sshKeys = [ ];
-              # Force kernel 6.12 - must be set here to override minimal CD
-              boot.kernelPackages = nixpkgs.lib.mkForce nixpkgs.legacyPackages.x86_64-linux.linuxPackages_6_12;
-            }
-          ];
-        };
-
-        # Test server configuration for nixos-anywhere deployment
-        test-server = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            disko.nixosModules.disko
-            ./modules/server
-            ./modules/disko-single-disk-root
-            ./vms/test-server/configuration.nix
-          ];
-        };
+    # ISO configuration without SSH keys (use bin/build-iso for SSH keys)
+    nixosConfigurations = {
+      keystoneIso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ./modules/iso-installer.nix
+          {
+            _module.args.sshKeys = [];
+            # Force kernel 6.12 - must be set here to override minimal CD
+            boot.kernelPackages = nixpkgs.lib.mkForce nixpkgs.legacyPackages.x86_64-linux.linuxPackages_6_12;
+          }
+        ];
       };
 
-      # Export Keystone modules for use in other flakes
-      nixosModules = {
-        server = ./modules/server;
-        client = ./modules/client;
-        clientHome = ./modules/client/home;
-        diskoSingleDiskRoot = ./modules/disko-single-disk-root;
-        isoInstaller = ./modules/iso-installer.nix;
-      };
-
-      packages.x86_64-linux = {
-        iso = self.nixosConfigurations.keystoneIso.config.system.build.isoImage;
+      # Test server configuration for nixos-anywhere deployment
+      test-server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./modules/server
+          ./modules/disko-single-disk-root
+          ./modules/initrd-ssh-unlock
+          ./vms/test-server/configuration.nix
+        ];
       };
     };
+
+    # Export Keystone modules for use in other flakes
+    nixosModules = {
+      server = ./modules/server;
+      client = ./modules/client;
+      clientHome = ./modules/client/home;
+      diskoSingleDiskRoot = ./modules/disko-single-disk-root;
+      initrdSshUnlock = ./modules/initrd-ssh-unlock;
+      isoInstaller = ./modules/iso-installer.nix;
+    };
+
+    packages.x86_64-linux = {
+      iso = self.nixosConfigurations.keystoneIso.config.system.build.isoImage;
+    };
+  };
 }
