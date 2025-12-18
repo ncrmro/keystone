7f8a5a049c5ba87e6cdaebe443f8d1eefa0e4389
Nicholas Romero <ncrmro@gmail.com>
2025-12-17 23:25:03 -0600

    chore: fix builds

--- packages/keystone-ha/tui/default.nix
+++ packages/keystone-ha/tui/default.nix
@@ -8,18 +8,22 @@ rustPlatform.buildRustPackage {
   pname = "keystone-ha-tui-client";
   version = "0.1.0";
 
-  src = ./.;
+  # Build from workspace root since tui depends on common crate
+  src = ./..;
 
-  cargoLock.lockFile = ./Cargo.lock;
+  cargoLock.lockFile = ./../Cargo.lock;
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ openssl ];
+  # Build only the tui crate from the workspace
+  buildAndTestSubdir = "tui";
+
+  nativeBuildInputs = [pkg-config];
+  buildInputs = [openssl];
 
   meta = with lib; {
     description = "TUI client for Keystone cross-realm resource management";
     homepage = "https://github.com/ncrmro/keystone";
     license = licenses.mit;
-    maintainers = [ ];
+    maintainers = [];
     mainProgram = "keystone-ha-tui";
   };
 }
